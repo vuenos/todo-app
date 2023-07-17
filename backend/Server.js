c2803147aec33656c16dde459398c93f93f9d39e
jintae kim <vuenos@naver.com>
2023-07-17 22:50:15 +0900

    set

--- backend/Server.js
+++ backend/Server.js
@@ -2,16 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js"
 
 
 dotenv.config();
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5500;
 
+app.use(cors());
 
 
 app.listen(PORT, () => {
-  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 
 })
